08c25949e006c427eb418974282eedb16d79753c
shoiyadesu! <akariondev@gmail.com>
2025-06-19 20:16:01 -0300

    Update twrp_bangkk.mk

--- twrp_bangkk.mk
+++ twrp_bangkk.mk
@@ -16,6 +16,9 @@
 # limitations under the License.
 #
 
+PRODUCT_RELEASE_NAME := bangkk
+DEVICE_PATH := device/motorola/$(PRODUCT_RELEASE_NAME)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
@@ -30,10 +33,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit some OFOX stuff.
 $(call inherit-product, $(DEVICE_PATH)/fox_bangkk.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := bangkk
-DEVICE_PATH := device/motorola/$(PRODUCT_RELEASE_NAME)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := bangkk
