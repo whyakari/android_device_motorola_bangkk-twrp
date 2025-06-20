d2c3283b763476f713c9b18ae8a9fb700e7e3eb6
shoiyadesu! <akariondev@gmail.com>
2025-06-19 21:15:25 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,6 @@ export FOX_VIRTUAL_AB_DEVICE=1
 export OF_FORCE_PREBUILT_KERNEL=1
 export FOX_BUILD_DEVICE=bangkk
 
-export FOX_VERSION="R12.1"
-export FOX_VARIANT="A14"
-export FOX_BUILD_TYPE="Stable"
+# export FOX_VERSION="R12.1"
+# export FOX_VARIANT="A14"
+# export FOX_BUILD_TYPE="Stable"
